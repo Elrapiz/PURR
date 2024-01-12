b41da63052add5fd9a65db73561306d747348edf
Elrapiz <rafael.pizarro.elrapiz@gmail.com>
2024-01-12 10:43:54 +0800

    Updated autoload.php

--- core/autoload.php
+++ core/autoload.php
@@ -3,5 +3,18 @@
 declare(strict_types = 1);
 
 spl_autoload_register(function ($classname) {
-    
+
+    $parts = explode('\\', $classname);
+    $class = array_pop($parts);
+    $parts = strtolower(implode(DS, $parts));
+
+    $path = '..' . DS . $parts . DS . $class . '.php';
+
+    if (file_exists($path)) {
+        require_once realpath($path);
+    } else {
+        // maybe do this only in debug or development mode? dunno :|
+        die("Path: {$path} does not exist");
+    }
+
 });
