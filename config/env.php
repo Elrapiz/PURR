45b00bc56320ac9030865c15dbb7faad630eac99
Elrapiz <rafael.pizarro.elrapiz@gmail.com>
2024-01-12 10:54:53 +0800

    Added constants for database configuration

--- config/env.php
+++ config/env.php
@@ -1,3 +1,8 @@
 <?php
 
 defined('DS') || define('DS', DIRECTORY_SEPARATOR);
+
+defined('DB_HOST') || define('DB_HOST', 'localhost');
+defined('DB_NAME') || define('DB_NAME', 'purr');
+defined('DB_USER') || define('DB_USER', 'root');
+defined('DB_PASSWORD') || define('DB_PASSWORD', '');
