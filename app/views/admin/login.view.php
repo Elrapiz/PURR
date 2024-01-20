4f888087207946b18b3927aaf11010e992880865
Elrapiz <rafael.pizarro.elrapiz@gmail.com>
2024-01-21 00:09:32 +0800

    Added the error prompt

--- app/views/admin/login.view.php
+++ app/views/admin/login.view.php
@@ -14,6 +14,11 @@
     <form class="form" action="/admin/login" method="post">
         <h1 class="form__title">PURR Administrator</h1>
         <div class="form__field">
+
+            <?php if(isset($data['errorMsg'])) : ?>
+                <p class="form__response-msg"><?php echo $data['errorMsg'] ?></p>
+            <?php endif; ?>
+
             <input class="form__username" type="text" name="username" placeholder="Username" required>
             <input class="form__password" type="password" name="password" placeholder="Password" required>
             <button class="form__login">Log in</button>
