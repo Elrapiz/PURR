551ac00a4862b7d5bea06b2a6b0999d8cd497586
Elrapiz <rafael.pizarro.elrapiz@gmail.com>
2024-01-20 11:16:18 +0800

    Added render and redirect function

--- core/functions.php
+++ core/functions.php
@@ -2,6 +2,18 @@
 
 declare(strict_types = 1);
 
+function render(string $viewPath, mixed $data = null) {
+    if(file_exists($viewPath)) {
+        require_once $viewPath;
+    } else {
+        die("View path: {$viewPath} does not exist");
+    }
+}
+
+function redirect(string $url) {
+    header("Location:/{$url}");
+}
+
 function dd(mixed $data, bool $die = true): void {
     echo '<br/><pre>';
     print_r($data);
