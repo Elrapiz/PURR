b70a8a00f58cdc0311caf771b980d2fe32f4da7d
Elrapiz <rafael.pizarro.elrapiz@gmail.com>
2024-01-21 00:10:21 +0800

    Updated the login process

--- app/controllers/admin/Login.php
+++ app/controllers/admin/Login.php
@@ -4,7 +4,16 @@ declare(strict_types = 1);
 
 namespace App\Controllers\Admin;
 
+use \App\Models\Admin;
+
 class Login {
+
+    private Admin $adminModel;
+
+    public function __construct() {
+        global $dbc;
+        $this->adminModel = new Admin($dbc);
+    }
 
     public function index() {
         // log in render
@@ -12,9 +21,16 @@ class Login {
     }
 
     public function login() {
-        // logging in process
-        if($_SERVER['REQUEST_METHOD'] === 'POST') {
-            echo 'POST process';
+        
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        if($this->adminModel->userExist($username, $password)) {
+            echo 'User exist';
+        } else {
+            render('../app/views/admin/login.view.php', [
+                'errorMsg' => 'Invalid username or password. Try again'
+            ]);
         }
 
     }
